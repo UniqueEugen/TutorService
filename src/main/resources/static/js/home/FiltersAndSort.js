7bfaa11c8465ed4ecbcc06b59c60fdff547634d7
UniqueEugen <112904113+UniqueEugen@users.noreply.github.com>
2025-01-16 03:35:36 +0300

    Подготовкка к диплому: Починил фильтрацию и сортировку. НИЧЕГО НЕ МЕНЯТЬ В КАРТОЧКАХ РЕПЕТИТОРОВ!!! Иначе опять полетит

--- src/main/resources/static/js/home/FiltersAndSort.js
+++ src/main/resources/static/js/home/FiltersAndSort.js
@@ -8,38 +8,39 @@ function applyFilterAndSort() {
     Array.from(tutors).forEach(function (tutor) {
         var tutorSpecialization = tutor.getElementsByClassName("card-title")[0].innerText.toLowerCase();
         var tutorName = tutor.getElementsByTagName("p")[0].innerText.toLowerCase();
-        var tutorCity = tutor.getElementsByTagName("p")[2].innerText.toLowerCase();
+        var tutorCity = tutor.getElementsByTagName("p")[tutor.getElementsByTagName("p").length-4].innerText.toLowerCase();
 
         if (
             (filterField === "specialization" && !tutorSpecialization.includes(filterValue)) ||
             (filterField === "lastName" && !tutorName.includes(filterValue)) ||
             (filterField === "city" && !tutorCity.includes(filterValue))
         ) {
+            console.log(tutor);
+            console.log(tutorCity);
             tutor.style.display = "none";
         } else {
             tutor.style.display = "block";
         }
     });
 
-    var tutorsContainer = document.getElementsByClassName("row")[1];
+    var tutorsContainer = document.getElementsByClassName("sortMy")[0];
     var tutorsArray = Array.from(tutorsContainer.getElementsByClassName("col-md-4"));
-
     if (sortOrder === "asc") {
+        console.log("I'm in ASC");
         tutorsArray.sort(function (a, b) {
-            console.log(a)
-            var price = a.getElementsByTagName("p")[a.getElementsByTagName("p").length-1].innerText.replace("Price: $", "");
+            console.log(a.getElementsByTagName("p"))
+            var price = a.getElementsByTagName("p")[a.getElementsByTagName("p").length-3].innerText.replace("Цена: $", "");
             console.log(price)
-            var tutorPriceA = parseFloat(a.getElementsByTagName("p")[a.getElementsByTagName("p").length-1].innerText.replace("Price: $", ""));
-            price = b.getElementsByTagName("p")[b.getElementsByTagName("p").length-1].innerText.replace("Price: $", "");
-            console.log(b)
+            var tutorPriceA = parseFloat(a.getElementsByTagName("p")[a.getElementsByTagName("p").length-3].innerText.replace("Цена: $", ""));
+            price = b.getElementsByTagName("p")[b.getElementsByTagName("p").length-3].innerText.replace("Цена: $", "");
             console.log(price)
-            var tutorPriceB = parseFloat(b.getElementsByTagName("p")[b.getElementsByTagName("p").length-1].innerText.replace("Price: $", ""));
+            var tutorPriceB = parseFloat(b.getElementsByTagName("p")[b.getElementsByTagName("p").length-3].innerText.replace("Цена: $", ""));
             return tutorPriceA - tutorPriceB;
         });
     } else if (sortOrder === "desc") {
         tutorsArray.sort(function (a, b) {
-            var tutorPriceA = parseFloat(a.getElementsByTagName("p")[a.getElementsByTagName("p").length-1].innerText.replace("Price: $", ""));
-            var tutorPriceB = parseFloat(b.getElementsByTagName("p")[b.getElementsByTagName("p").length-1].innerText.replace("Price: $", ""));
+            var tutorPriceA = parseFloat(a.getElementsByTagName("p")[a.getElementsByTagName("p").length-3].innerText.replace("Цена: $", ""));
+            var tutorPriceB = parseFloat(b.getElementsByTagName("p")[b.getElementsByTagName("p").length-3].innerText.replace("Цена: $", ""));
             return tutorPriceB - tutorPriceA;
         });
     }
